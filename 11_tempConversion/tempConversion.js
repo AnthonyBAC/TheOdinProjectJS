d45760c88a68ceb695db0ae224899e10ac3046f8
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-05-12 19:44:30 -0400

    2

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  const convertToC = (f - 32) * 5 / 9;
+  return parseFloat(convertToC.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  const convertToF = (c * 9 / 5) + 32;
+  return parseFloat(convertToF.toFixed(1));
 };
 
 // Do not edit below this line
