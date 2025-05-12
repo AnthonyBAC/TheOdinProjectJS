c9e3769f87cb987ba68e3161990fca19e7b773f0
Anthony <106450012+AnthonyBAC@users.noreply.github.com>
2025-05-11 22:48:10 -0400

    2

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let result = ''
+    for (let i = 0; i < num; i++){
+        result += string;
+    }
+    
+    if (num < 0 ){
+        return 'ERROR'
+    } else {
+        return result;
+    }
+    
+    
 };
 
+repeatString('hey',3)
 // Do not edit below this line
 module.exports = repeatString;
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    return array.filter( !values.includes(item));
 };
 
+console.log(removeFromArray([1, 2, 3, 4], 3)); 
 // Do not edit below this line
 module.exports = removeFromArray;
